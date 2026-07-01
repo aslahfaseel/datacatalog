dd2e6e1180986d52b15fcd602d6caf6008dc2404
aslahfaseel-k_mcdcorp <aslahfaseel.k@us.mcd.com>
2026-07-01 18:25:35 +0530

    added

--- vz/envs/dev/cloudrun.tf
+++ vz/envs/dev/cloudrun.tf
@@ -8,12 +8,10 @@ module "cloudrun_aspect_patcher" {
   container_image       = var.aspect_patcher_image
   service_account_email = var.terraform_sa
 
-  # Pipeline 1: Asset Governance (reads CSV from GCS)
   gcs_bucket_name     = var.aspect_patcher_gcs_bucket
   gcs_csv_path        = "data/vz_aspect_assignment.csv"
   dataplex_project_id = var.project_id
 
-  # Pipeline 2: Data Classification (reads DLP results from BigQuery)
   governance_project = var.project_id
   curated_project    = var.project_id
   dlp_results_table  = "${var.project_id}.vzdataset.sdp_results"
